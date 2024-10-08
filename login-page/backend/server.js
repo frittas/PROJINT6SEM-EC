3ec9e6275f8826dc111918c2c18c9a3ec0aeea4f
Gabriel Henrique <gabrielhsilva10@gmail.com>
2024-10-07 23:08:30 -0300

    navegacao para dashboard implementada

--- login-page/backend/server.js
+++ login-page/backend/server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const mysql = require('mysql2');
+const cors = require('cors'); // Adicione o cors
 
 const app = express();
 app.use(express.json());
+app.use(cors()); // Habilitar CORS
 
 // Conectar ao MySQL
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'murilo', //Colocar a senha do seu sql
+  password: 'user1232024', //Colocar a senha do seu MySQL
   database: 'projbase' //Nome do Schema para teste
 });
 
@@ -21,14 +23,25 @@ db.connect((err) => {
 // Rota de login
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
+  if (!username || !password) {
+    // Verifica se username e password foram enviados
+    return res.status(400).json({ message: 'Por favor, forneça username e password' });
+  }
 
   const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
   db.query(query, [username, password], (err, results) => {
-    if (err) throw err;
+    if (err) {
+      console.error('Erro na consulta ao MySQL:', err);
+      return res.status(500).json({ message: 'Erro interno no servidor' });
+    }
+
     if (results.length > 0) {
-      res.json({ message: 'Login bem-sucedido' });
+      // Usuário encontrado
+      return res.status(200).json({ message: 'Login bem-sucedido' });
+      
     } else {
-      res.status(401).json({ message: 'Usuário ou senha incorretos' });
+      // Usuário não encontrado
+      return res.status(401).json({ message: 'Usuário ou senha incorretos' });
     }
   });
 });

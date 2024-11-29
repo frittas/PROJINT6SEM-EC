8c4e4dd78970c75d81b3488655edb0d2cb39dd84
Gabriel Henrique <gabrielhsilva10@gmail.com>
2024-11-29 12:03:20 -0300

    tentativa de corrigir o erro de navigate

--- rams/src/pages/dashboard/dashboard.jsx
+++ rams/src/pages/dashboard/dashboard.jsx
@@ -1,34 +1,42 @@
 import React, { useState } from 'react';
 import './dashboard.css';
 import Map from './map';
-import { FaBars, FaUser} from 'react-icons/fa';
+import { FaBars, FaUser } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom'; // Usar o hook useNavigate para redirecionar
 
 function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false); // Sidebar state
+  const navigate = useNavigate(); // Inicializa o hook para navegação
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen); // Toggle sidebar open/close
   };
 
+  const redirectToLogin = () => {
+    navigate("/"); // Redireciona para a página inicial
+  };
+
   return (
-    <div className='dashboard'>
+    <div className="dashboard">
       {/* Header */}
-      <div className='header'>
-        <FaBars className='barras' onClick={toggleSidebar} />
-        <h1 className='header-title'>RAMS</h1>
-        <FaUser className='usuario' style={{ marginLeft: 'auto' }} />
+      <div className="header">
+        <FaBars className="barras" onClick={toggleSidebar} />
+        <h1 className="header-title">RAMS</h1>
+        <FaUser className="usuario" style={{ marginLeft: 'auto' }} />
       </div>
 
       {/* Sidebar */}
       <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
-        <ul className='sidebarButtons'>
-          <li className='sidebarSair'>Sair</li>
+        <ul className="sidebarButtons">
+          <button type="button" className="sidebarSair" onClick={redirectToLogin}>
+            Sair
+          </button>
         </ul>
       </div>
 
       {/* Main Content */}
       <div
-        className='mapa'
+        className="mapa"
         style={{ marginLeft: isSidebarOpen ? '350px' : '0' }} // Ajusta a margem com base no estado da sidebar
       >
         <Map />
--- rams/src/pages/dashboard/map.jsx
+++ rams/src/pages/dashboard/map.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { FaSearch} from 'react-icons/fa';
+import { FaSearch } from 'react-icons/fa';
 
 const Map = () => {
     const [mapLoaded, setMapLoaded] = useState(false);
@@ -13,182 +13,170 @@ const Map = () => {
     const [messageBody, setMessageBody] = useState('');
     const [snackbarMessage, setSnackbarMessage] = useState('');
 
-    const radiusMapping = { // Mapeando distancias com seus respectivos valores
+    const mapContainerRef = useRef(null); // Ref para o container do mapa
+    const radiusMapping = {
         '1km': 1000,
         '5km': 5000,
         '10km': 10000,
     };
 
-    // Initialize the map and circle
     useEffect(() => {
-        const initMap = () => {
-            const initialLocation = { lat: -23.550520, lng: -46.633308 }; // São Paulo
-
-            const newMap = new window.google.maps.Map(document.getElementById('map'), {
-                zoom: 12,
-                center: initialLocation,
-            });
-            setMap(newMap);
-
-            const newCircle = new window.google.maps.Circle({
-                map: newMap,
-                center: initialLocation,
-                radius: circleRadius,
-                fillColor: '#FF0000',
-                fillOpacity: 0.35,
-                strokeWeight: 2,
-                editable: true,
-                draggable: true,
-            });
-
-            setCircle(newCircle);
-
-            // Move o centro do mapa quando o circulo for arrastado
-            newCircle.addListener('dragend', () => {
-                const newCenter = newCircle.getCenter();
-                setCircleCenter({ lat: newCenter.lat(), lng: newCenter.lng() });
-                newMap.panTo(newCenter);
-            });
-
-            //  Atualiza o raio do circulo quando ele for alterado
-            newCircle.addListener('radius_changed', () => {
-                const newRadius = newCircle.getRadius();
-                setCircleRadius(newRadius);
-
-                // Atualiza o checkbox correspondente 
-                // Verifica se o raio atual corresponde ao raio do mapeamento
-                const matchingDistance = Object.entries(radiusMapping).find(
-                    ([, radius]) => radius === newRadius
-                );
-
-                if (matchingDistance) { // Se o raio atual corresponder
-                    setSelectedDistance(matchingDistance[0]); // Checa o checkbox correspondente
-                } else {
-                    setSelectedDistance(null); // Limpa o checkbox
-                }
-            });
-
-            // Adiciona a barra de pesquisa ao mapa e configura o autocomplete
-            const autocomplete = new window.google.maps.places.Autocomplete(searchInputRef.current);
-            autocomplete.bindTo('bounds', newMap);
+        // Define a função initMap globalmente
+        window.initMap = () => {
             
-            autocomplete.addListener('place_changed', () => {
-                const place = autocomplete.getPlace();
-                if (!place.geometry || !place.geometry.location) return; // Manuseio de erro caso o lugar nao seja encontrado
+            if (mapContainerRef.current) {
+                const initialLocation = { lat: -23.550520, lng: -46.633308 }; // São Paulo
 
-                newMap.panTo(place.geometry.location); //Move o mapa para o lugar
-                newMap.setZoom(15);
-                newCircle.setCenter(place.geometry.location); //Move o circulo para o lugar
-                setCircleCenter({ lat: place.geometry.location.lat(), lng: place.geometry.location.lng() }); //Atualiza o centro do circulo
-            });
-        };
+                const newMap = new window.google.maps.Map(mapContainerRef.current, {
+                    zoom: 12,
+                    center: initialLocation,
+                });
+                setMap(newMap);
 
-        const loadScript = () => { // Carrega o script do google maps
-            if (!document.querySelector('script[src*="maps.googleapis.com"]')) { // Verifica se o script ja foi carregado
-                const script = document.createElement('script'); // Cria o script
-                script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&libraries=places&callback=initMap`; // URL do script
-                script.async = true;
-                script.defer = true;
-                script.setAttribute("loading", "lazy");  // Adds loading=lazy
-                document.body.appendChild(script); // Adiciona o script
+                const newCircle = new window.google.maps.Circle({
+                    map: newMap,
+                    center: initialLocation,
+                    radius: circleRadius,
+                    fillColor: '#FF0000',
+                    fillOpacity: 0.35,
+                    strokeWeight: 2,
+                    editable: true,
+                    draggable: true,
+                });
 
-                script.onload = () => setMapLoaded(true); // Define que o mapa foi carregado
-            } else {
-                setMapLoaded(true); // Define que o mapa foi carregado
+                setCircle(newCircle);
+
+                newCircle.addListener('dragend', () => {
+                    const newCenter = newCircle.getCenter();
+                    setCircleCenter({ lat: newCenter.lat(), lng: newCenter.lng() });
+                    newMap.panTo(newCenter);
+                });
+
+                newCircle.addListener('radius_changed', () => {
+                    const newRadius = newCircle.getRadius();
+                    setCircleRadius(newRadius);
+
+                    const matchingDistance = Object.entries(radiusMapping).find(
+                        ([, radius]) => radius === newRadius
+                    );
+
+                    if (matchingDistance) {
+                        setSelectedDistance(matchingDistance[0]);
+                    } else {
+                        setSelectedDistance(null);
+                    }
+                });
+
+                const autocomplete = new window.google.maps.places.Autocomplete(searchInputRef.current);
+                autocomplete.bindTo('bounds', newMap);
+                autocomplete.addListener('place_changed', () => {
+                    const place = autocomplete.getPlace();
+                    if (!place.geometry || !place.geometry.location) return;
+
+                    newMap.panTo(place.geometry.location);
+                    newMap.setZoom(15);
+                    newCircle.setCenter(place.geometry.location);
+                    setCircleCenter({ lat: place.geometry.location.lat(), lng: place.geometry.location.lng() });
+                });
+
+                setMapLoaded(true); // Sinaliza que o mapa foi carregado
             }
         };
 
-        if (!window.google) { // Verifica se o google maps ja foi carregado na pagina
-            loadScript(); // Carrega o script do google maps caso nao tenha sido carregado
-            window.initMap = initMap; // Define initMap como propriedade global
-        } else {
-            initMap(); // inicia o mapa com os parametros definidos
-        }
+        const loadScript = () => {
+            if (!document.querySelector('script[src*="maps.googleapis.com"]')) {
+                const script = document.createElement('script');
+                script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&libraries=places&callback=initMap`;
+                script.async = true;
+                script.defer = true;
+                script.setAttribute("loading", "lazy");
+                document.body.appendChild(script);
+        
+                script.onload = () => {
+                    console.log("Google Maps script loaded");
+                    setMapLoaded(true);
+                };
+            } else {
+                setMapLoaded(true);
+            }
+        };
+        
+
+        loadScript();
 
         return () => {
             delete window.initMap; // Limpa a função global
         };
     }, []);
 
-    // Manuseia mudanças no checkbox
     const handleCheckboxChange = (distance) => {
         setSelectedDistance(distance);
-
         if (circle) {
-            const currentCenter = circle.getCenter(); // Get o centro atual
-            circle.setRadius(radiusMapping[distance]); // Set o novo raio
-            circle.setCenter(currentCenter); // Set o centro atual
+            const currentCenter = circle.getCenter();
+            circle.setRadius(radiusMapping[distance]);
+            circle.setCenter(currentCenter);
         }
     };
 
-    const jsonData = { // cria o json com as informacoes necessarias para a busca e notificaçao
+    const jsonData = {
         message: messageBody,
         title: messageTitle,
-        latitude: Number(circleCenter.lat),  
-        longitude: Number(circleCenter.lng), 
-        radius: Number(circleRadius), 
+        latitude: Number(circleCenter.lat),
+        longitude: Number(circleCenter.lng),
+        radius: Number(circleRadius),
     };
-    
-    const showSnackbar = (messageCode) => { // Mostra o snackbar
+
+    const showSnackbar = (messageCode) => {
         const snackbar = document.getElementById("snackbar");
         snackbar.className = "show";
-        setTimeout(() => { // Esconde o snackbar apos 3 segundos
-          snackbar.className = snackbar.className.replace("show", "");
+        setTimeout(() => {
+            snackbar.className = snackbar.className.replace("show", "");
         }, 3000);
-        if (messageCode == 1) // Verifica o codigo da mensagem
-        {
+
+        if (messageCode === 1) {
             setSnackbarMessage(`Alerta enviado com sucesso!`);
-        }
-        else if (messageCode == 2)
-        {
+        } else if (messageCode === 2) {
             setSnackbarMessage(`Erro ao enviar alerta!`);
-        }
-        else if (messageCode == 3)
-        {
+        } else if (messageCode === 3) {
             setSnackbarMessage(`Preencha todos os campos!`);
-        }
-        else
-        { 
+        } else {
             setSnackbarMessage(`Erro ao enviar alerta!`);
         }
-      };
-    
-    // Manuseia o envio do alerta
+    };
+
     const handleSendAlert = async () => {
-        if (!messageTitle || !messageBody) { // Verifica se os campos estao preenchidos
-            showSnackbar(3); // Mostra a mensagem de erro caso os campos nao estao preenchidos
+        if (!messageTitle || !messageBody) {
+            showSnackbar(3);
             return;
         }
         try {
-            const response = await fetch('http://localhost:3000/push', { // Envia os dados para o servidor
+            const response = await fetch('http://localhost:3000/push', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify(jsonData),  // Stringify o json para enviar
+                body: JSON.stringify(jsonData),
             });
-    
-            if (!response.ok) { // Verifica se a resposta foi bem sucedida
-                showSnackbar(2); // Mostra a mensagem de erro caso a resposta nao for bem sucedida
+
+            if (!response.ok) {
+                showSnackbar(2);
                 throw new Error(`Server error: ${response.status}`);
+            } else {
+                showSnackbar(1);
             }
-            else {
-                showSnackbar(1); // Mostra a mensagem de sucesso
-            }
-            const result = await response.json(); 
+
+            const result = await response.json();
             console.log('Response from server:', result);
         } catch (error) {
             console.error('Error sending data:', error);
             showSnackbar(2);
         }
     };
-    
 
     if (!mapLoaded) return <p>Carregando Mapa...</p>;
 
     return (
         <div style={{ height: '100%', width: '100%' }}>
-            {/* Search bar */}
             <div className="searchBarContainer">
                 <FaSearch className='searchIcon'/>
                 <input
@@ -199,32 +187,24 @@ const Map = () => {
                 />
             </div>
             <div className='inputContainer'>
-                {/* Title input */}
                 <input
                     className='inputMessageTitle'
                     type="text"
                     placeholder="Titulo"
                     value={messageTitle}
                     onChange={(e) => setMessageTitle(e.target.value)}
-                    ></input>
-
-                {/* Message input */}
+                />
                 <input
                     className='inputMessage'
                     type="text"
                     placeholder="Mensagem"
                     value={messageBody}
                     onChange={(e) => setMessageBody(e.target.value)}
-                    ></input>
+                />
             </div>
-            {/* Map */}
-            <div 
-            id="map"
-            style={{ height: '80%', width: '100%' }}
-            
-            ></div>
 
-            {/* Radius options */}
+            <div ref={mapContainerRef} style={{ height: '80%', width: '100%' }}></div>
+
             <div className='optionsDashboard' style={{ padding: '10px' }}>
                 <label>
                     <input
@@ -256,12 +236,9 @@ const Map = () => {
                 Enviar Alerta
             </button>
 
-            {/* Snackbar*/}
             <div id="snackbar">
-		        <p>
-			        {snackbarMessage}
-		        </p>
-	        </div>
+                <p>{snackbarMessage}</p>
+            </div>
         </div>
     );
 };
